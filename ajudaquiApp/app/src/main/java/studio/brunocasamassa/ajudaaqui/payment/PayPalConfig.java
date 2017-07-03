ff764f18d94789943fab5d0c1959e8ea8c21b6c4
brunocasamassa <brunocasamassa@hotmail.com>
2017-07-03 18:37:02 -0300

    djkjd

---
+++ ajudaquiApp/app/src/main/java/studio/brunocasamassa/ajudaaqui/payment/PayPalConfig.java
@@ -0,0 +1,11 @@
+package studio.brunocasamassa.ajudaaqui.payment;
+
+/**
+ * Created by bruno on 02/07/2017.
+ */
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "ATFvkQQh0SClpuxWjIglQkjJ-dpfilhXAhQ-LhSrjAYEogkS_qkUhpD5EigtwzEczgWaUHfH5RJkhXnj";
+    /*public static final String PAYPAL_CLIENT_ID =  "AS_WGho9jdqNSsWSG__i1Yzeecm4CUV2ZLNTPfsGHg30762peM7-7I213gJW8OM1zSDfIHf_saC0FK6H";*/
+
+}
---
+++ ajudaquiApp/app/src/main/java/studio/brunocasamassa/ajudaaqui/payment/PaymentActivity3.java
@@ -0,0 +1,139 @@
+package studio.brunocasamassa.ajudaaqui.payment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
+
+import studio.brunocasamassa.ajudaaqui.R;
+
+/**
+ * Created by bruno on 02/07/2017.
+ */
+
+public class PaymentActivity3 extends AppCompatActivity  implements View.OnClickListener{
+
+    //The views
+    private ImageButton buttonPay;
+    private TextView editTextAmount;
+
+    //Payment Amount
+    private String paymentAmount = "9.99";
+    //Paypal Configuration Object
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paypal);
+
+        buttonPay = (ImageButton) findViewById(R.id.buttonPay);
+        editTextAmount = (TextView) findViewById(R.id.editTextAmount);
+
+        buttonPay.setOnClickListener(this);
+
+        Intent intent = new Intent(this, PayPalService.class);
+
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        startService(intent);
+
+    }
+
+      //COMPRA DIRETA
+        private static PayPalConfiguration config = new PayPalConfiguration()
+                // Start with mock environment.  When ready, switch to sandbox (ENVIRONMENT_SANDBOX)
+                // or live (ENVIRONMENT_PRODUCTION)
+                .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+                .clientId(PayPalConfig.PAYPAL_CLIENT_ID);
+
+    private void getPayment() {
+        //Getting the amount from editText
+
+        //Creating a paypalpayment
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(paymentAmount)), "BRL", "Simplified Coding Fee",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+
+        //Creating Paypal Payment activity intent
+        Intent intent = new Intent(PaymentActivity3.this, com.paypal.android.sdk.payments.PayPalFuturePaymentActivity.class);
+
+        //putting the paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        //Puting paypal payment to the intent
+        intent.putExtra(com.paypal.android.sdk.payments.PaymentActivity.EXTRA_PAYMENT, payment);
+
+        //Starting the intent activity for result
+        //the request code will be used on the method onActivityResult
+        startActivityForResult(intent, PayPalFuturePaymentActivity.RESULT_OK);
+
+    }
+
+    public static final int PAYPAL_REQUEST_CODE = 123;
+
+    @Override
+    public void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+   @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //If the result is from paypal
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+
+            //If the result is OK i.e. user has not canceled the payment
+            if (resultCode == Activity.RESULT_OK) {
+
+
+                //Getting the payment confirmation
+                PaymentConfirmation confirm = data.getParcelableExtra(com.paypal.android.sdk.payments.PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+
+                //if confirmation is not null
+                if (confirm != null) {
+
+                    try {
+
+                        //Getting the payment details
+                        String paymentDetails = confirm.toJSONObject().toString(4);
+                        Log.i("paymentExample", paymentDetails);
+
+                        //Starting a new activity for the payment details and also putting the payment details with intent
+                        startActivity(new Intent(this, ConfirmationActivity.class)
+                                .putExtra("PaymentDetails", paymentDetails)
+                                .putExtra("PaymentAmount", paymentAmount));
+
+                    } catch (JSONException e) {
+                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("paymentExample", "The user canceled.");
+            } else if (resultCode == com.paypal.android.sdk.payments.PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        getPayment();
+    }
+
+
+
+    //--
+}
